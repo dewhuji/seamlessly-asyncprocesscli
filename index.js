a7a41b1f86a909f109c4447dc8e04fcaf714c54f
dewhuji <gsjcabriellegar@hotmail.com>
2024-05-09 04:11:38 +0700

    init project

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
-  }
-  return [...map.values()];
+const fruits = ["apple", "banana", "orange"];
+for (const fruit of fruits) {
+  console.log(fruit);
 }
